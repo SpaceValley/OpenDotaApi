b5efc56c25260fc820bd6cbe692978a0f9e30899
SpaceValley <123kmv123@gmail.com>
2019-03-08 13:01:03 +0200

    heroes stats added

--- src/components/HeroesList/HeroesList.js
+++ src/components/HeroesList/HeroesList.js
@@ -5,39 +5,72 @@ import * as actions from "../../actions";
 import history from "../../history";
 
 
-
-const HeroesList  = ({heroes}) =>{
+const HeroesList = ({heroes}) => {
     console.log(heroes);
-    const backToLogIn = () =>{
+    const backToLogIn = () => {
         history.push('/');
     };
 
     return (
         <div className="heroes-list">
-             <button className='btn btn-primary logOut-btn'
-                        onClick={(e) => {
-                            e.preventDefault();
-                            backToLogIn();
-                        }}>
-                    Log Out
-                </button>
-                <h1 className="text-white text-center mb-5">Heroes List</h1>
-                <ul className='list-group heroes-ul'>
-                    {heroes.map((hero,i) => 
+            <button className='btn btn-dark logOut-btn'
+                    onClick={(e) => {
+                        e.preventDefault();
+                        backToLogIn();
+                    }}>
+                Log Out
+            </button>
+            <h1 className="text-white text-center mb-5">Heroes List</h1>
+            <ul className='list-group heroes-ul'>
+                {heroes.map((hero, i) =>
                     <li key={i} className="list-group-item mx-2 my-2 hero-li">
-                        <img src={`http://api.opendota.com${hero.img}`} className="hero-img" alt={hero.localized_name}/>
+                            <img src={`http://api.opendota.com${hero.img}`} className="hero-img"
+                                 alt={hero.localized_name}/>
+                            <div>
+                                <p className="hero-name text-center mt-2">{hero.localized_name}</p>
+                                <p className="hero-attack text-center my-2">{hero.attack_type}</p>
+                            </div>
+
                         <div className="hero-infoWrap">
-                            <p className="hero-name">{hero.localized_name}</p>
-                            <p className="hero-attack mt-2 mb-0">attack_type :</p>
-                            <p className="hero-attack m-0">{hero.attack_type}</p>
-                            <p className="hero-attack mt-2 mb-0">attack_rate :</p>
-                            <p className="hero-attack m-0">{hero.attack_rate}</p>
-                            <p className="hero-attack mt-2 mb-0">move_speed :</p>
-                            <p className="hero-attack m-0">{hero.move_speed}</p>
+                            <div>
+                                <div className="stat-wrap">
+                                    <img src="http://cdn.dota2.com/apps/dota2/images/heropedia/overviewicon_int.png"
+                                         alt="int"/>
+                                    <span className="hero-stat">{hero.base_int} + {hero.int_gain}</span>
+                                </div>
+                                <div className="stat-wrap">
+                                    <img src="http://cdn.dota2.com/apps/dota2/images/heropedia/overviewicon_agi.png"
+                                         alt="agi"/>
+                                    <span className="hero-stat">{hero.base_agi} + {hero.agi_gain}</span>
+                                </div>
+                                <div className="stat-wrap">
+                                    <img src="http://cdn.dota2.com/apps/dota2/images/heropedia/overviewicon_str.png"
+                                         alt="str"/>
+                                    <span className="hero-stat">{hero.base_str} + {hero.str_gain}</span>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="stat-wrap">
+                                    <img src="http://cdn.dota2.com/apps/dota2/images/heropedia/overviewicon_attack.png"
+                                         alt="attack"/>
+                                    <span
+                                        className="hero-stat">{hero.base_attack_min} - {hero.base_attack_max}</span>
+                                </div>
+                                <div className="stat-wrap">
+                                    <img src="http://cdn.dota2.com/apps/dota2/images/heropedia/overviewicon_speed.png"
+                                         alt="speed"/>
+                                    <span className="hero-stat">{hero.move_speed}</span>
+                                </div>
+                                <div className="stat-wrap">
+                                    <img src="http://cdn.dota2.com/apps/dota2/images/heropedia/overviewicon_defense.png"
+                                         alt="defense"/>
+                                    <span className="hero-stat mr-4">{hero.base_armor}</span>
+                                </div>
+                            </div>
                         </div>
                     </li>)}
-                </ul>
-               
+            </ul>
+
         </div>
     );
 };
